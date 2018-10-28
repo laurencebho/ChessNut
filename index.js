c2750bbc2cb8ec81c3f0aaa3d6e07fe81ba2d973
laurencebho <laurence.ho@durham.ac.uk>
2018-10-28 01:02:41 +0100

    update to login

--- index.js
+++ index.js
@@ -20,50 +20,62 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 
 app.get('/', (req, res)=> {
   res.render('index.pug');
-})
+});
 //const users = {'admin': new User('admin', ...)};
 const admin = new Schema({
 	username: {
 		type: String,
 		required: true,
-		enum: ['admin']
+		length: {min: 3, max: 16}
 	},
 	password: {
 		type: String,
 		required: true,
-		enum: ['guest']
+		length: {min: 5, max: 20}
 	}
 });
 
-app.route('/login')
-	.get((req, res)=> {
-		res.render('login.pug')   
-	})
-	.post((req, res)=> {
-		let data = {username: req.body.username, password: req.body.password};
-		let errors = admin.validate(data);
-		if (errors.length == 0) {
-			res.cookie('username', data.username, {maxAge: 18000, httpOnly: true});
-			res.render('index.pug');
+app.get('/partials/login', (req, res)=> {
+		console.log('received request for partial');
+		res.render('loginform.pug');
+});
+
+app.post('/login', (req, res)=> {
+	let data = {username: req.body.username, password: req.body.password};
+	console.log(data);
+	let errors = admin.validate(data);
+	if (errors.length == 0) {
+		if (data.username == 'admin' && data.password == 'guest') {
+			console.log("login success");
+			res.cookie('username', data.username, {maxAge: 18000, signed: true, httpOnly: true});
+			res.json({page: 'home'
+			});
+			return;
 		}
-		else {
-			res.redirect('/')
-		}
-	});
+	}
+	res.status(400).json({message: 'Incorrect credentials, please try again'});
+});
 
 app.get('/logout', (req, res)=> {
 	res.clearCookie('username');
-	res.render('index.pug');
+	res.json({
+	});
 });
 
-app.get('/me', (req, res)=> {
-	if (logged_in) {
-		res.json({...});
-	} else {
-		res.status(400).json(...);
-	}
+app.get('/create', loggedIn, (req, res)=> {
+	res.json({});
 });
+
+
+function loggedIn(req, res, next) {
+	if (req.signedCookies.username == 'admin') {
+		return next();
+	}
+	res.status(400).json('You must be signed in as admin to view this page');	
+}
+
+//middleware to check if authentication needed
--- public/script.js
+++ public/script.js
@@ -1,8 +1,24 @@
 $.ajaxSetup({contentType: 'application/json; charset=utf-8', dataType: 'json'});
 
 $.hashroute("/login", ()=> {
-	$("#main-content").empty().append("loginform.pug");
-	$("#loginform").submit(()=> {
+	$("#main-content").empty();
+	$.get("/partials/login", (data)=> {
+		$("#main-content").html(data);
+	}, 'html');
 
-	})
-})
+	$("#login-button").click(()=> {
+		console.log("submitting form");
+		$.ajax({
+			method: "POST",
+			url: "/login",
+			data: {username: $("#username").value(), password: $("#password").value()},
+			success: (data)=> {
+				renderHome(data);
+			}
+		});
+	});
+});
+
+function renderHome(data) {
+	console.log(data);
+}
