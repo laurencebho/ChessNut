56158ad914f7aca4319435ca1761bab312472e64
Laurie <laurence.ho@durham.ac.uk>
2019-01-16 12:23:16 +0000

    Change port for hosting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app.js');
 const http = require('http').Server(app);
 const port = 5000;
 
-http.listen(port, (err) => {
+http.listen(process.env.PORT || port, (err) => {
 	if (err) return console.log(err);
 	return console.log('Listening on port ' + port);
 });
